9d52efa5476f4883eb23f9cbfa200e750e909c23
driedfruit <driedfruit@mindloop.net>
2015-07-09 03:32:12 +0400

    Minimize drawing area.

--- paint.lua
+++ paint.lua
@@ -29,8 +29,12 @@ function draw_tiles(map)
 	local level = cLevel
 	local tilesets = cLevel.tilesets
 
-	for j = 1, level.rows do
-		for i = 1, level.cols do
+	local tx,ty,tw,th = camera:getTileBounds()
+
+	local func_draw = love.graphics.draw
+
+	for j = ty, th do
+		for i = tx, tw do
 
 			local tileid = map[j][i]
 			local x = (i-1) * level.tileW
@@ -38,11 +42,11 @@ function draw_tiles(map)
 
 			if tileid > 0 then
 
-				if tileid == 384 then
+				--if tileid == 384 then
 				--printf("Drawing tile %d at %d, %d\n", tileid, x, y)
-				end
+				--end
 
-				love.graphics.draw(tilesets[level.tileset_id]['image'], tilesets[level.tileset_id]['quads'][tileid], x, y)
+				func_draw(tilesets[level.tileset_id]['image'], tilesets[level.tileset_id]['quads'][tileid], x, y)
 
 			end
 		end
